2d41f766ab669432cdb8da64fd6c9a0e12f13bc0
Hans Song <hans.m.song@gmail.com>
2023-01-23 18:22:55 +1000

    flesh out ping

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,4 +1,5 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { getVoiceConnection } from '@discordjs/voice';
+import { EmbedBuilder, SlashCommandBuilder } from 'discord.js';
 
 import { Command } from '../lib/commands';
 
@@ -8,8 +9,14 @@ export const ping: Command = {
     .setDescription('Check if bot is healthy and configured correctly'),
 
   onMessage: async ({ client }, message) => {
-    const ping = client.ws.ping;
-    const latency = Date.now() - message.createdTimestamp;
-    await message.channel.send(`pong\nping: ${ping}, latency: ${latency}`);
+    const voice = message.guild && getVoiceConnection(message.guild.id);
+    const embed = new EmbedBuilder().setDescription('pong').addFields([
+      { name: 'API ping', value: `${client.ws.ping}ms` },
+      { name: 'API latency', value: `${Date.now() - message.createdTimestamp}ms` },
+      { name: 'Voice ping (ws)', value: `${voice ? `${voice.ping.ws}ms` : 'N/A'}` },
+      { name: 'Voice ping (udp)', value: `${voice ? `${voice.ping.udp}ms` : 'N/A'}` },
+    ]);
+
+    await message.channel.send({ embeds: [embed] });
   },
 };
